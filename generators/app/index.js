0bd78f73b56977b4188471a64c575906b0f1e424
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2017-09-18 16:06:13 +0200

    create prompt for a simple test

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,18 +1,51 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-    /*// The name `constructor` is important here
+    // The name `constructor` is important here
     constructor(args, opts) {
-      // Calling the super constructor is important so our generator is correctly set up
-      super(args, opts);
-  
-      // Next, add your custom code
-      this.option('babel'); // This method adds support for a `--babel` flag
-    }
-    methodTest() {
+        // Calling the super constructor is important so our generator is correctly set up
+        super(args, opts);
+
+        this.argument('appname', { type: String, required: true });
+        this.argument('version', { type: String, required: false });
+        this.argument('description', { type: String, required: false });
+        this.argument('username', { type: String, required: false });
+      
+        // And you can then access it later; e.g.
+        this.log(this.options.appname);
+        // Next, add your custom code
+        //this.option('babel'); // This method adds support for a `--babel` flag
 
     }
-    methodeTestTwo(){
 
-    }*/
-  };
+
+    prompting() {
+        return this.prompt([{
+          type    : 'input',
+          name    : 'name',
+          message : 'Name of the project'
+        },{
+            type    : 'input',
+            name    : 'version',
+            message : 'Version of the project'
+        },
+        {
+            type    : 'input',
+            name    : 'description',
+            message : 'Description of the project'
+        }, {
+            type    : 'input',
+            name    : 'username',
+            message : 'What\'s your GitHub username',
+            store   : true
+          }]).then((answers) => {
+            this.log('app name', answers.name);
+            this.log('version', answers.version);
+            this.log('description', answers.description);
+            this.log('author', answers.username);
+            this.log(this.destinationRoot());    
+    
+        });
+      }
+};
+
