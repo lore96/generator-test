c27c04bfafb7780504d6e442636f86eefa38bc67
Lorenzo Mussi <l.mussi96@gmail.com>
2017-09-20 12:19:29 +0200

    generators app test prompt and value check

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,45 +7,82 @@ module.exports = class extends Generator {
         super(args, opts);
 
         this.argument('appname', { type: String, required: true });
-        this.argument('version', { type: String, required: false });
-        this.argument('description', { type: String, required: false });
-        this.argument('username', { type: String, required: false });
-      
-        // And you can then access it later; e.g.
-        this.log(this.options.appname);
-        // Next, add your custom code
-        //this.option('babel'); // This method adds support for a `--babel` flag
 
-    }
+        this.appConfiguration = {};
 
-
-    prompting() {
-        return this.prompt([{
-          type    : 'input',
-          name    : 'name',
-          message : 'Name of the project'
-        },{
+        this.prompts = [{
+            type    : 'input',
+            name    : 'name',
+            message : 'Name of the project'
+        }, {
             type    : 'input',
             name    : 'version',
             message : 'Version of the project'
-        },
-        {
+        }, {
             type    : 'input',
             name    : 'description',
             message : 'Description of the project'
         }, {
             type    : 'input',
-            name    : 'username',
-            message : 'What\'s your GitHub username',
-            store   : true
-          }]).then((answers) => {
-            this.log('app name', answers.name);
-            this.log('version', answers.version);
-            this.log('description', answers.description);
-            this.log('author', answers.username);
-            this.log(this.destinationRoot());    
-    
+            name    : 'keywords',
+            message : 'Keywords'
+        }, {
+            type    : 'input',
+            name    : 'license',
+            message : 'License'
+        }, {
+            type    : 'input',
+            name    : 'giturl',
+            message : 'Git url'
+        }, {
+            type    : 'input',
+            name    : 'authors',
+            message : 'Author'
+        }, {
+            type    : 'input',
+            name    : 'mainFile',
+            message : 'Main file'
+        }, {
+            type    : 'input',
+            name    : 'globalname',
+            message : 'Global name (to be exported in windows)'
+        }];
+    }
+
+    prompting(){
+        return this.prompt(this.prompts).then((answers) => {
+            if(!answers.giturl || !answers.authors){
+                console.log('ERROR: can not proceed. Name, Git url or authors not provided!');
+                return;
+            }
+
+            if(!answers.name){
+                answers.name = this.options.appname;
+            }
+
+            if(!answers.version){
+                answers.version = '0.1.0';
+            }
+
+            if(!answers.license){
+                answers.license = 'MIT';
+            }
+
+            if(!answers.mainfile){
+                answers.mainfile = 'main.js'
+            }
+
+            if(!answers.globalname){
+                answers.globalname = answers.name
+            }
+
+            this.appConfiguration = answers;
         });
-      }
+    }
+
+    test(){
+        this.log(this.destinationRoot());        
+        this.log(this.appConfiguration);
+    }
 };
 
